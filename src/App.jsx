ae4167aa9a7d24eb7f0a7f74621e6dc763f67fa2
iftakhar <iftakharalamshuvo11@gmail.com>
2024-03-22 15:08:14 +0600

    initial added

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Form from "./components/Form/Form"
+
+
+function App() {
+  
+
+  return (
+    <>
+     
+      <h1 className="text-center md:text-5xl text-2xl text-teal-400 font-bold my-10">Information Table </h1>
+    <div className="w-2/4 mx-auto">
+      <Form></Form>
+    </div>
+    
+    </>
+  )
+}
+
+export default App
