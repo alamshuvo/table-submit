f0c26f52e0137c25f9292bae02eb182e3d717971
iftakhar <iftakharalamshuvo11@gmail.com>
2024-03-22 15:26:22 +0600

    animate and value access

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Form from "./components/Form/Form"
+import 'animate.css';
 
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
   return (
     <>
      
-      <h1 className="text-center md:text-5xl text-2xl text-teal-400 font-bold my-10">Information Table </h1>
+      <h1 className="text-center md:text-5xl text-2xl text-teal-400 font-bold my-10 animate__animated animate__backInDown animate__repeat-2">Information Table </h1>
     <div className="w-2/4 mx-auto">
       <Form></Form>
     </div>
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,9 +1,34 @@
+import { useState } from "react";
+
 
 
 const Form = () => {
+const [name,setName]=useState("")
+
+
+const handleSubmit=e=>{
+    e.preventDefault();
+    // setName();
+    console.log(e.target.name.value);
+    console.log(e.target.email.value);
+    console.log(e.target.password.value);
+}
+
+
+
+
     return (
         <div>
-            
+            <form onSubmit={handleSubmit}>
+                <input className="w-full outline-dotted" type="text" name="name" />
+                <br />
+                <br />
+                <input className="w-full outline-dashed" type="email" name="email" required />
+                <br />
+                <br />
+                <input type="password" className="w-full outline-dashed" name="password" />
+                <button className="btn btn-primary mt-5 ">submit</button>
+            </form>
         </div>
     );
 };
