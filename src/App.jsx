2f70b4813b1ace7236cb6942b9f59e208f1e50d6
iftakhar <iftakharalamshuvo11@gmail.com>
2024-03-22 16:00:26 +0600

    some code added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import Form from "./components/Form/Form"
-import 'animate.css';
+
 
 
 function App() {
   
 
   return (
-    <>
+    <div className="bg-purple-200 p-10 min-h-screen">
      
-      <h1 className="text-center md:text-5xl text-2xl text-teal-400 font-bold my-10 animate__animated animate__backInDown animate__repeat-2">Information Table </h1>
+   <div >
+   <h1 className="text-center md:text-5xl text-2xl text-teal-400 font-bold my-10 animate__animated animate__backInDown animate__repeat-2">Information Table </h1>
     <div className="w-2/4 mx-auto">
       <Form></Form>
     </div>
+   </div>
+   </div>
     
-    </>
   )
 }
 
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,17 +1,38 @@
 import { useState } from "react";
+import 'animate.css';
 
 
 
 const Form = () => {
-const [name,setName]=useState("")
+const [name,setName]=useState("Hablu");
+const [email,setEmail]=useState("Hablu@gmail.com");
+const [password,setPassword]=useState("");
+const [error,setError]=useState('');
 
-
+// console.log(name,email,password);
 const handleSubmit=e=>{
     e.preventDefault();
     // setName();
-    console.log(e.target.name.value);
-    console.log(e.target.email.value);
-    console.log(e.target.password.value);
+    // console.log(e.target.name.value);
+    // console.log(e.target.email.value);
+    // console.log(e.target.password.value);
+  
+    if (password.length<6) {
+        setError("password shuld  must be 6 char")
+    }
+   else{
+    setError("")
+    console.log(name,email,password);
+   }
+}
+const handleNameChange=e=>{
+    setName(e.target.value);
+}
+const handleEmailChange=e=>{
+    setEmail(e.target.value);
+}
+const handlePasswordChange=e=>{
+    setPassword(e.target.value);
 }
 
 
@@ -20,14 +41,26 @@ const handleSubmit=e=>{
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input className="w-full outline-dotted" type="text" name="name" />
+                <input
+                value={name}
+                onChange={handleNameChange}
+                className="w-full outline-dotted" type="text" name="name" />
                 <br />
                 <br />
-                <input className="w-full outline-dashed" type="email" name="email" required />
+                <input 
+                value={email}
+                onChange={handleEmailChange}
+                className="w-full outline-dashed" type="email" name="email" required />
                 <br />
                 <br />
-                <input type="password" className="w-full outline-dashed" name="password" />
-                <button className="btn btn-primary mt-5 ">submit</button>
+                <input type="password"
+                value={password}
+                onChange={handlePasswordChange}
+                className="w-full outline-dashed" name="password" required />
+                <button className="btn btn-primary mt-5 ">submit</button>;
+                {
+                    error && <p className=" animate__animated animate__backInUp text-3xl font-bold text-red-500">{error}</p>
+                }
             </form>
         </div>
     );
